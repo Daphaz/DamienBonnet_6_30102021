d7fff84982a7f101e091b536f9f05a280e6c4762
Damien Bonnet <bonnet.damien@ymail.com>
2021-10-30 08:58:22 +0200

    [:sparkles: FEAT]  screen and components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import useTags from "../hooks/useTags";
+
+const NavItem = ({ ariaLabel, name, tabindex, onClick }) => {
+	return (
+		<li className="header__navItem">
+			<button
+				className="link"
+				aria-label={ariaLabel}
+				tabIndex={tabindex}
+				onClick={() => onClick(name)}>
+				#{name}
+			</button>
+		</li>
+	);
+};
+
+const Navbar = ({ onClick }) => {
+	const { tags } = useTags();
+	return (
+		<nav className="header__nav" aria-label="photographer categories">
+			<ul role="navigation" className="header__list" aria-label="liste de tags">
+				{tags.map((tag) => (
+					<NavItem key={`header-navitem-${tag.name}`} {...tag} onClick={onClick} />
+				))}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
